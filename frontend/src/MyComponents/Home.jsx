28a3dafa2eb38a6e9eb484b8a3d4442f67f041d4
Mukesh Sahani <mukeshsahani@exafluence.com>
2023-10-10 15:58:05 +0530

    index.htlm

--- frontend/src/MyComponents/Home.jsx
+++ frontend/src/MyComponents/Home.jsx
@@ -6,7 +6,15 @@ export const Home = () => {
             <div>
                 <h1>Exafluence Shipment Tracking System</h1>
                 <br />
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos quam natus ipsum ut nemo nobis dolorum asperiores, ratione animi. Sint, quisquam cumque nisi illum libero fugit. Sequi aperiam alias ullam! Dolorum aperiam cupiditate fugit ab tenetur ea, aspernatur dolore praesentium voluptas corporis natus, consectetur at debitis doloribus quos aut doloremque facere. Magnam beatae voluptates laudantium quibusdam tempore, assumenda adipisci pariatur cupiditate ab sint laboriosam delectus commodi sunt dolores magni neque numquam debitis. Placeat quisquam dignissimos sapiente doloribus totam. Tempora, vero. Accusantium veritatis ipsum laboriosam eum facilis consequuntur reprehenderit ducimus dolor, culpa ea blanditiis quibusdam optio natus autem fuga totam. Tempore.</p>
+                <p>Live shipment tracking helps improve the supply chain management process. 
+                    Buyers and sellers can gain visibility into transit times, identify potential bottlenecks, and optimize their operational processes. 
+                    Visibility helps drive improved efficiency and cost-effectiveness, both for the shipment in progress and future shipments. 
+
+                    Shipment tracking also plays a role in customer satisfaction. Even if shipments are delayed, 
+                    real-time updates regarding a shipment supply customers with a better understanding of why their goods have been delayed. 
+                    This transparency allows them to plan accordingly and gives them a measure of control. 
+                    Offering live shipment tracking insights to customers can significantly build trust and foster a strong, 
+                    long-term relationship. </p>
                 <br />
             </div>
         </div>
